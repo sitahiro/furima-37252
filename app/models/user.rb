7f95109dad1f64c103e2489ffb7410c5942bb6e2
sitahiro <geed5141@gmail.com>
2021-12-23 23:06:09 +0900

    モデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,21 @@
 class User < ApplicationRecord
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_day, presence: true
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :birth_day, presence: true
+  validates :password, format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+    validates :family_name
+    validates :first_name
+  end
+  with_options presence: true, format: {with: /\A[ァ-ヶー]+\z/} do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
   
   has_many :items
   has_many :cards
