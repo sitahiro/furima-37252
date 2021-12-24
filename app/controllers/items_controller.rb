55bfe6bc81c1756e5ac2dce1b9fbf7048c8ca3a9
sitahiro <geed5141@gmail.com>
2021-12-24 12:41:41 +0900

    管理機能　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,15 +3,15 @@ class ItemsController < ApplicationController
   def index
   end
 
-  def new
-  end
+  #def new
+  #end
 
-  def create
-  end
+  #def create
+  #end
 
-  def edit
-  end
+  #def edit
+  #end
 
-  def update
-  end
+  #def update
+  #end
 end
