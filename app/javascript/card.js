b77082307b15144ad5001a1dd46d1f0a0b412762
sitahiro <geed5141@gmail.com>
2022-01-05 17:22:32 +0900

    JavaScriptでtokenを生成してサーバーサイドへ送信

---
+++ app/javascript/card.js
@@ -0,0 +1,26 @@
+const pay = () => {
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("購入ボタンを押した際にイベント発火")
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order[number]"),
+      cvc: formData.get("order[cvc]"),
+      exp_month: formData.get("order[exp_month]"),
+      exp_year: `20${formData.get("order[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
