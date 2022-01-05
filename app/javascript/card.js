79a9a920e79908a1c710714e6093a52bae8d29cc
sitahiro <geed5141@gmail.com>
2022-01-05 21:24:13 +0900

    トークンをサーバーサイドへ送る

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -21,8 +21,14 @@ const pay = () => {
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type="hidden"> `;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-        debugger;
       }
+
+      document.getElementById("order_number").removeAttribute("name");
+      document.getElementById("order_cvc").removeAttribute("name");
+      document.getElementById("order_exp_month").removeAttribute("name");
+      document.getElementById("order_exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
     });
   });
 };
