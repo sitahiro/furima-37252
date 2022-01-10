d5cb0100532b70cee23486656179871800571c5f
sitahiro <geed5141@gmail.com>
2022-01-10 11:44:26 +0900

    出品ページにて、エラーハンドリングの際、重複したエラーメッセージを表示しない

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,8 +16,8 @@ class Item < ApplicationRecord
     validates :explanation, length: { maximum: 1000 }
     validates :price
     validates :price,
-              numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
-    validates :price, numericality: { only_integer: true, message: 'Half-width number' }
+              numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }, allow_blank: true
+    validates :price, numericality: { only_integer: true, message: 'Half-width number' }, allow_blank: true
     validates :category_id, numericality: { other_than: 1 }
     validates :situation_id, numericality: { other_than: 1 }
     validates :cost_id, numericality: { other_than: 1 }
